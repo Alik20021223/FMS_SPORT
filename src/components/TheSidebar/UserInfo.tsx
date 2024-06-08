6c3256bf133238938ae9efcbd10d733d8bc456e8
Abdulloh PC <sabirovabdulloh2004@gmail.com>
2024-06-08 07:19:14 +0500

    1

--- src/components/TheSidebar/UserInfo.tsx
+++ src/components/TheSidebar/UserInfo.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link';
 import { AppButton } from '@/components/core/Button/AppButton';
 import { useRouter } from 'next/navigation';
 import { PersonalState } from '@/app/interfaces/Person';
+import { useAppSelector } from '@/redux/hooks';
+import { useEffect, useState } from 'react';
 
 export type TUserInfoData = {
   fullname: string;
@@ -15,32 +17,37 @@ type TUserInfoProps = {
   data: PersonalState
 }
 
-const userMenu = [
-  { name: 'Персональные данные', link: '/profile' },
-  { name: 'Антропометрия', link: '/profile/anthropometry' },
-  { name: 'Семья', link: '/profile/family' },
-  { name: 'Достижения', link: '/profile/achievements' },
-  { name: 'Мой клуб', link: '/my-club' },
-];
-
-
-const menu = [
-  { name: 'Клубы', link: '/clubs' },
-  { name: 'Спортсмены', link: '/athletes' },
-  { name: 'Турниры', link: '/tournaments' },
-  { name: 'Настройки', link: '/setting' },
-  { name: 'Обратная связь', link: '/feedback' },
-];
-
 export const UserInfo = ({ data }: TUserInfoProps) => {
   const router = useRouter();
+  const user: PersonalState = useAppSelector(state => state.personal)
+  const [menu, setMenu] = useState<any[]>([
+    { name: 'Клубы', link: '/clubs' },
+    { name: 'Спортсмены', link: '/athletes' },
+    { name: 'Турниры', link: '/tournaments' },
+    { name: 'Настройки', link: '/setting' },
+    { name: 'Обратная связь', link: '/feedback' },
+  ])
+  const [userMenu, setUserMenu] = useState<any[]>([
+    { name: 'Персональные данные', link: '/profile', isActive: true},
+    { name: 'Антропометрия', link: '/profile/anthropometry', isActive: true },
+    { name: 'Семья', link: '/profile/family', isActive: true },
+    { name: 'Достижения', link: '/profile/achievements', isActive: true },
+    { name: 'Мой клуб', link: '/my-club', isActive: user.club ? true : false },
+  ]);
+
+  useEffect(() => {
+    
+  }, [])
+
   const redirectToPay = () => {
     router.push('/pay');
   };
 
+
   const handleRedirect = (path: string) => {
     router.push(path);
   };
+
 
   return (
     <div >
@@ -67,7 +74,7 @@ export const UserInfo = ({ data }: TUserInfoProps) => {
           <li>
             <Link href={'/profile'}>Личные данные</Link>
             <ul className="flex flex-col gap-3 ml-10 mt-4">
-              {userMenu.map((m, i) => (
+              {userMenu.map((m, i) => m.isActive ? (
                 <li key={i}>
                   <Link href={m.link} prefetch={false}>
                     <div onClick={() => handleRedirect(m.link)}>
@@ -75,7 +82,7 @@ export const UserInfo = ({ data }: TUserInfoProps) => {
                     </div>
                   </Link>
                 </li>
-              ))}
+              ) : (<></>))}
             </ul>
           </li>
           {menu.map((m, i) => (
